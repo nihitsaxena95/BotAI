8b9c59f720dd052ace8a1fd1241c42714d1be5da
nihitsaxena95 <nihitsaxena95@gmail.com>
2017-11-22 14:19:15 +0530

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -3,7 +3,7 @@ module.exports={
 	url : 'mongodb://localhost:27017/learningbot',
 	neo4jurlpassword: 'jasmine9',
 	neo4jUrl : "bolt://localhost",
-	//clientRedirectUrl : "http://13.90.139.94:4200"
-	clientRedirectUrl : "http://localhost:4200" 
+	clientRedirectUrl : "http://13.59.184.134:4200"
+	//clientRedirectUrl : "http://localhost:4200" 
 	// url:'mongodb://admin:admin@192.168.252.203:27020/'
 }
